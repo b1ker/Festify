f6832e21b7142eac9eb3da538649d6fdef61416b
Pawel Kaniewski <pawel@kaniewski.eu>
2023-04-27 17:11:52 -0500

    Update to use edgesql on Mac Mq

--- Scripts/startsql.sh
+++ Scripts/startsql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Pass@word1' -p 1433:1433 \
--v sqlvolume:/var/opt/mssql \
--d mcr.microsoft.com/mssql/server:2019-latest
+docker \
+run --cap-add SYS_PTRACE -e 'ACCEPT_EULA=1' -e 'MSSQL_SA_PASSWORD=Pass@word1' -p 1433:1433 \
+--name sqledge -d mcr.microsoft.com/azure-sql-edge
